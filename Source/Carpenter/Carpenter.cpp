5f3db7a5c1fa0ec99487c4b209c56eb6e5ca5d68
serhanozalp <85905439+serhanozalp@users.noreply.github.com>
2025-03-20 18:07:27 +0300

    Initial Commit

---
+++ Source/Carpenter/Carpenter.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Carpenter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Carpenter, "Carpenter" );
