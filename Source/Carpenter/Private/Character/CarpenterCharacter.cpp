ee42f710bb7a77282f934a6804a723dc872614c1
serhanozalp <85905439+serhanozalp@users.noreply.github.com>
2025-03-20 19:05:08 +0300

    Assets

---
+++ Source/Carpenter/Private/Character/CarpenterCharacter.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Carpenter/Public/Character/CarpenterCharacter.h"
+
+
+// Sets default values
+ACarpenterCharacter::ACarpenterCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ACarpenterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACarpenterCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ACarpenterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/Carpenter/Public/Character/CarpenterCharacter.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Carpenter/TP_ThirdPerson/TP_ThirdPersonCharacter.h"
+#include "CarpenterCharacter.generated.h"
+
+UCLASS()
+class CARPENTER_API ACarpenterCharacter : public ATP_ThirdPersonCharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ACarpenterCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
